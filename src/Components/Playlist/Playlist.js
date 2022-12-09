e5e8dafa743c44840797726eae504595241880af
y1lmaz-Ahmet <ay0201998@gmail.com>
2022-12-09 13:01:07 +0100

    showing tracks in playlist and results

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import "./Playlist.css";
+import TrackList from "../TrackList/TrackList";
 export default class Playlist extends Component {
   render() {
     return (
       <div className="Playlist">
         <input defaultValue={"New Playlist"} />
-        {/* <!-- Add a TrackList component --> */}
+        <TrackList tracks={this.props.playlistTracks} />
         <button className="Playlist-save">SAVE TO SPOTIFY</button>
       </div>
     );
--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 import "./TrackList.css";
+import Track from "../Track/Track";
 export default class TrackList extends Component {
   render() {
     return (
       <div className="TrackList">
-        {/* <!-- You will add a map method that renders a set of Track components  --> */}
+        {this.props.tracks.map((track) => {
+          return <Track track={track} key={track.id} />;
+        })}
       </div>
     );
   }
