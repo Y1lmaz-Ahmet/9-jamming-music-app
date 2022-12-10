0210ed04abb59c391181d9b41278795b8d262bee
y1lmaz-Ahmet <ay0201998@gmail.com>
2022-12-10 15:57:14 +0100

    Implement Spotify Search Request

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,6 +4,8 @@ import "./App.css";
 import { SearchBar } from "../SearchBar/SearchBar";
 import SearchResults from "../SearchResults/SearchResults";
 import Playlist from "../Playlist/Playlist";
+import Spotify from "../../util/Spotify";
+
 export class App extends React.Component {
   constructor(props) {
     super(props);
@@ -92,7 +94,9 @@ export class App extends React.Component {
     const trackUris = this.state.playlistTracks.map((track) => track.uri);
   }
   search(term) {
-    console.log(term);
+    Spotify.search(term).then((searchResults) => {
+      this.setState({ searchResults: searchResults });
+    });
   }
 
   render() {
--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,3 +1,52 @@
-const Spotify = {};
+const clientId = "50027cc8775f43fe9e658b981eee31b8";
+const redirectUri = "http://localhost:3000";
+let accesToken;
+const Spotify = {
+  getAccesToken() {
+    if (accesToken) {
+      return accesToken;
+    }
+
+    // check for an acces token match:
+    const accesTokenMatch = window.location.href.match(/acces_token=([^&]*)/);
+    const expiresInMatch = window.location.href(/expires_in=([^&]*)/);
+
+    if (accesTokenMatch && expiresInMatch) {
+      accesToken = accesTokenMatch[1];
+      const expiresIn = Number(expiresInMatch[1]);
+
+      //this clears the parameter, allowing us to grab a new acces token when it expires:
+      window.setTimeout(() => (accesToken = ""), expiresIn * 1000);
+      window.history.pushState("acces Token", null, "/");
+      return accesToken;
+    } else {
+      const accesUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
+      window.location = accesUrl;
+    }
+  },
+  search(term) {
+    const accesToken = Spotify.getAccesToken();
+    return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
+      headers: {
+        Authorization: `Bearer ${accesToken}`,
+      },
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((jsonResponse) => {
+        if (!jsonResponse.tracks) {
+          return [];
+        }
+        return jsonResponse.tracks.items.map((track) => ({
+          id: track.id,
+          name: track.name,
+          artist: track.artists[0].name,
+          album: track.album.name,
+          uri: track.uri,
+        }));
+      });
+  },
+};
 
 export default Spotify;
