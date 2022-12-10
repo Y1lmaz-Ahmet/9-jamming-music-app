57df00e09ea36a77ea5ab63fe07c5785f2f4ebce
y1lmaz-Ahmet <ay0201998@gmail.com>
2022-12-10 15:59:34 +0100

    updated method to async

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -24,28 +24,27 @@ const Spotify = {
       window.location = accesUrl;
     }
   },
-  search(term) {
+  async search(term) {
     const accesToken = Spotify.getAccesToken();
-    return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
-      headers: {
-        Authorization: `Bearer ${accesToken}`,
-      },
-    })
-      .then((response) => {
-        return response.json();
-      })
-      .then((jsonResponse) => {
-        if (!jsonResponse.tracks) {
-          return [];
-        }
-        return jsonResponse.tracks.items.map((track) => ({
-          id: track.id,
-          name: track.name,
-          artist: track.artists[0].name,
-          album: track.album.name,
-          uri: track.uri,
-        }));
-      });
+    const response = await fetch(
+      `https://api.spotify.com/v1/search?type=track&q=${term}`,
+      {
+        headers: {
+          Authorization: `Bearer ${accesToken}`,
+        },
+      }
+    );
+    const jsonResponse = await response.json();
+    if (!jsonResponse.tracks) {
+      return [];
+    }
+    return jsonResponse.tracks.items.map((track) => ({
+      id: track.id,
+      name: track.name,
+      artist: track.artists[0].name,
+      album: track.album.name,
+      uri: track.uri,
+    }));
   },
 };
 
